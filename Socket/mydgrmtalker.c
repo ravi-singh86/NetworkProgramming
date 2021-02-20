d7136ada3cc8332b78645c3e9deaec857ab471a8
Ravi <shanker86@gmail.com>
2021-02-20 16:17:30 -0500

    update

--- Socket/mydgrmtalker.c
+++ Socket/mydgrmtalker.c
@@ -27,48 +27,48 @@ void *get_in_addr(struct sockaddr *sa)
 
 
 int main(int argc, char* argv[]){
-	struct addrinfo hint, *res, *addrp;
-	int sockfd;
-	char s[INET6_ADDRSTRLEN];
+    struct addrinfo hint, *res, *addrp;
+    int sockfd;
+    char s[INET6_ADDRSTRLEN];
 
-	if(argc <2){
-		printf("Usage: ./taskname serverAddress");
-		return 1;
-	}
+    if(argc <2){
+        printf("Usage: ./taskname serverAddress");
+        return 1;
+    }
 
-	memset(&hint, 0, sizeof(hint));
-	hint.ai_family = AF_INET;
-	hint.ai_socktype = SOCK_DGRAM;
+    memset(&hint, 0, sizeof(hint));
+    hint.ai_family = AF_INET;
+    hint.ai_socktype = SOCK_DGRAM;
 
-	int rc = getaddrinfo(argv[1],PORT, &hint, &res);
-	if(rc != 0 ){
-		perror("getAddrinfo error:");
-		return 1;
-	}
-	
-	for(addrp = res; addrp != NULL; addrp = addrp->ai_next){
-		sockfd = socket(addrp->ai_family, addrp->ai_socktype, addrp->ai_protocol);
-		if(sockfd == -1){
-			perror("sockfd");
-			continue;
-		}
-		break;
-	}
+    int rc = getaddrinfo(argv[1],PORT, &hint, &res);
+    if(rc != 0 ){
+        perror("getAddrinfo error:");
+        return 1;
+    }
+    
+    for(addrp = res; addrp != NULL; addrp = addrp->ai_next){
+        sockfd = socket(addrp->ai_family, addrp->ai_socktype, addrp->ai_protocol);
+        if(sockfd == -1){
+            perror("sockfd");
+            continue;
+        }
+        break;
+    }
 
-	if(addrp == NULL){
-		printf("client: failed to connect\n");
-		return 2;
-	}
+    if(addrp == NULL){
+        printf("client: failed to connect\n");
+        return 2;
+    }
 
-	inet_ntop(addrp->ai_family, get_in_addr(addrp->ai_addr), s, sizeof(s));
-	printf("client: connecting to %s\n", s);
-	
-	const char* msg = "Hello There I am fast I send and forget\n";
-	int bytes = sendto(sockfd, msg, strlen(msg), 0, addrp->ai_addr, addrp->ai_addrlen);
+    inet_ntop(addrp->ai_family, get_in_addr(addrp->ai_addr), s, sizeof(s));
+    printf("client: connecting to %s\n", s);
+    
+    const char* msg = "Hello There I am fast I send and forget\n";
+    int bytes = sendto(sockfd, msg, strlen(msg), 0, addrp->ai_addr, addrp->ai_addrlen);
 
-	printf("sent %d bytes of data which is %s \n", bytes, msg);
-	freeaddrinfo(res);
+    printf("sent %d bytes of data which is %s \n", bytes, msg);
+    freeaddrinfo(res);
 
-	close(sockfd);
-	return 0;
+    close(sockfd);
+    return 0;
 }
